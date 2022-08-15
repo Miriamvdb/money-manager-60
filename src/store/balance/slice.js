88c42e19d9a617ea91865402660b6161b853880c
Miriamvdb <miriamvdb@live.nl>
2022-08-15 13:58:15 +0200

    Setup react-redux / deposit balance

---
+++ src/store/balance/slice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  amount: 0,
+};
+
+export const balanceSlice = createSlice({
+  name: "balance",
+  initialState,
+  reducers: {
+    deposit: (state, action) => {
+      console.log("Hello from reducer");
+      state.amount = state.amount + action.payload;
+    },
+  },
+});
+
+export const { deposit } = balanceSlice.actions;
+
+export default balanceSlice.reducer;
